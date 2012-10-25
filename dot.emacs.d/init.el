84e0e43d77847f58a92b7d995b8196735b8ef825
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-10-25 11:48:25 +0900

    add swap-screen
    分割した画面を入れ替える

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -485,6 +485,28 @@ html-helper-build-new-buffer is set to t")
     (setq truncate-lines t))
   (recenter))
 (global-set-key "\C-c\C-l" 'toggle-truncate-lines) ; 折り返し表示ON/OFF
+
+;;汎用機の SPF (mule みたいなやつ) には
+;;画面を 2 分割したときの 上下を入れ替える swap screen
+;;というのが PF 何番かにわりあてられていました。
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+(global-set-key [f2] 'swap-screen)
+(global-set-key [S-f2] 'swap-screen-with-cursor)
+
 ;; yasnippet
 ;(require 'yasnippet)
 ;(yas/global-mode 1)
